8acacac8552f959dcb5cf94798363355e85d2d87
vincentas12 <66995036+vincentas12@users.noreply.github.com>
2023-04-02 09:15:23 +0300

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,28 @@
+﻿{
+	"version": 1678019517,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/me-sheet0.png",
+		"images/other-sheet0.png",
+		"images/bullet-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"images/8899591fcde45ec3991ef48efb053e-sheet0.png",
+		"images/tiledbackground.png",
+		"media/a_game_inside_of_a_game.m4a",
+		"media/a_game_inside_of_a_game.ogg",
+		"media/b423b42.m4a",
+		"media/b423b42.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"Photon-Javascript_SDK.min.js"
+	]
+}
